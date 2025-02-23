1d4bbc73048afb5b4d833910392b31fb0c1cffc4
Ganthology <ray.gan@ytldigital.com>
2025-02-23 22:21:44 +0800

    fix: fix sign pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { SignUp } from "@clerk/nextjs";
- 
+
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
