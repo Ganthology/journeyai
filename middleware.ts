d0f54f79bbcd2ca6849e7febbc9033303a2f10d0
Ganthology <ray.gan@ytldigital.com>
2025-02-23 22:24:05 +0800

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
+export default clerkMiddleware();
+
+export const config = {
   publicRoutes: ["/api/webhooks(.*)"],
-  // Routes that can always be accessed, and have
-  // no authentication information
   ignoredRoutes: ["/api/webhooks(.*)"],
   afterAuth(auth, req) {
     // Handle users who aren't authenticated
@@ -23,8 +22,5 @@ export default authMiddleware({
       return Response.redirect(new URL("/record", req.url));
     }
   },
-});
-
-export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
